c2b77b768b1374c6f229539da65aed2280aa2ec9
Anna Sebastine <65461848+SemoneSeb@users.noreply.github.com>
2021-05-27 10:17:40 +0530

    Branch history register

---
+++ pipeline/4_stage_with_branchprediction/src/bhr.v
@@ -0,0 +1,26 @@
+module bhr #(parameter REGSIZE=2)(
+input       clk,
+input       rstn,
+input       bhr_en,
+input       bhr_in,
+
+output reg [REGSIZE-1:0] bhr_out
+);
+
+
+integer i;
+
+always@(posedge clk or negedge rstn)
+begin
+if(~rstn)
+  bhr_out <= 0;
+else 
+  begin
+    if(bhr_en)
+       bhr_out <= {bhr_in, bhr_out[REGSIZE-1:1]};
+    else
+       bhr_out <= bhr_out;
+  end
+end
+
+endmodule
