5f061c97a31a636316115ab9ad6c9bb5cc662f84
Anna Sebastine <65461848+SemoneSeb@users.noreply.github.com>
2021-06-20 23:36:30 +0530

    Added headers

--- pipeline/4_stage_with_branchprediction/src/PatternHistoryTable.v
+++ pipeline/4_stage_with_branchprediction/src/PatternHistoryTable.v
@@ -1,4 +1,14 @@
-//Owned by Praseeda S, Parvathy PH, Sanjana AR and Anna Sebastine
+/********************************************************************************************************
+Github repo : 313849252
+Date : 20/05/2021
+Authors : Praseeda S, Sanjana AR, Parvathy PH, Anna Sebastine
+College Name : College of Engineering Trivandrum
+Project Name : Vriddhi : Design and Verification of RISC-V core
+Design name : Pattern History Table 
+Module name : PatternHistoryTable 
+Description : Consists of 2^n entries per branch, each entry containing a two-bit saturating counter
+********************************************************************************************************/
+
 module PatternHistoryTable #(parameter REGSIZE=2)(
 input wire clk,
 input wire rstn,
