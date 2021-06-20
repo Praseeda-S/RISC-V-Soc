85947f5a95e7a229a0a83e781d6577f5c0f39095
Anna Sebastine <65461848+SemoneSeb@users.noreply.github.com>
2021-06-20 23:36:27 +0530

    Added headers

--- pipeline/4_stage_with_branchprediction/src/BranchTargetBuffer.v
+++ pipeline/4_stage_with_branchprediction/src/BranchTargetBuffer.v
@@ -1,4 +1,14 @@
-//Owned by Praseeda S, Parvathy PH, Sanjana AR and Anna Sebastine
+/***********************************************************************************************************
+Github repo : 313849252
+Date : 20/05/2021
+Authors : Praseeda S, Sanjana AR, Parvathy PH, Anna Sebastine
+College Name : College of Engineering, Trivandrum
+Project Name : Vriddhi : Design and Verification of RISC-V core
+Design name : Branch Target Buffer 
+Module name : BranchTargetBuffer  
+Description : Consists of destination of a branch and a history of whether the branch was taken
+***********************************************************************************************************/
+
 module BranchTargetBuffer #(parameter BLOCKSIZE = 4)(
 input       clk,
 input       rstn,
