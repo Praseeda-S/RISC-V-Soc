3e9c0ca6cc8098626c08dee932bd916a74180264
Anna Sebastine <65461848+SemoneSeb@users.noreply.github.com>
2021-06-20 23:36:38 +0530

    Added headers

--- pipeline/4_stage_with_branchprediction/src/exe.v
+++ pipeline/4_stage_with_branchprediction/src/exe.v
@@ -1,4 +1,14 @@
-//Owned by Praseeda S, Parvathy PH, Sanjana AR and Anna Sebastine
+/********************************************************************************************************************************************
+Github repo : 313849252
+Date : 20/05/2021
+Authors : Praseeda S, Sanjana AR, Parvathy PH, Anna Sebastine
+College Name : College of Engineering Trivandrum
+Project Name : Vriddhi : Design and Verification of RISC-V core
+Design name : Execution unit
+Module name : exe
+Description : Performs operations and calculations on the operands obtained from register set based on the control signals from decoder unit
+********************************************************************************************************************************************/
+
 module exe #(parameter WIDTH=32)(
 input wire clk,
 input wire rstn,
