3390f6a63cf49862077ee8a6f337b19076a0dc90
Anna Sebastine <65461848+SemoneSeb@users.noreply.github.com>
2021-06-20 23:36:21 +0530

    Added headers

--- pipeline/4_stage_with_branchprediction/src/BranchHistoryRegister.v
+++ pipeline/4_stage_with_branchprediction/src/BranchHistoryRegister.v
@@ -1,4 +1,14 @@
-//Owned by Praseeda S, Parvathy PH, Sanjana AR and Anna Sebastine
+/***********************************************************************************************************
+Github repo : 313849252
+Date : 20/05/2021
+Authors : Praseeda S, Sanjana AR, Parvathy PH, Anna Sebastine
+College Name : College of Engineering, Trivandrum
+Project Name : Vriddhi : Design and Verification of RISC-V core
+Design name : Branch History Register
+Module name : BranchHistoryRegister
+Description : A shift register that records last n branches encountered by the processor
+***********************************************************************************************************/
+
 module BranchHistoryRegister #(parameter REGSIZE=2)(
 input  clk,
 input  rstn,
