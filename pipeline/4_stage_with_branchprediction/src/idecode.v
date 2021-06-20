380ee9054138ac554a8372c3c75c5cb51f1b6b15
Anna Sebastine <65461848+SemoneSeb@users.noreply.github.com>
2021-06-20 23:36:43 +0530

    Added headers

--- pipeline/4_stage_with_branchprediction/src/idecode.v
+++ pipeline/4_stage_with_branchprediction/src/idecode.v
@@ -1,4 +1,14 @@
-//Owned by Praseeda S, Parvathy PH, Sanjana AR and Anna Sebastine
+/********************************************************************************************************
+Github repo : 313849252
+Date : 20/05/2021
+Authors : Praseeda S, Sanjana AR, Parvathy PH, Anna Sebastine
+College Name : College of Engineering Trivandrum
+Project Name : Vriddhi : Design and Verification of RISC-V core
+Design name : Instruction Decode 
+Module name : idecode
+Description : Generates control signals after taking inputs from ROM and instruction fetch unit
+********************************************************************************************************/
+
 module idecode(
 input wire clk,
 input wire rstn,
