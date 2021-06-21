21656cb7530e3994da9ce35616b7178094329cd8
Anna Sebastine <65461848+SemoneSeb@users.noreply.github.com>
2021-06-21 10:49:26 +0530

    Added headers

--- pipeline/4_stage_with_branchprediction/tb/riscv_tb.v
+++ pipeline/4_stage_with_branchprediction/tb/riscv_tb.v
@@ -1,4 +1,14 @@
-//Owned by Praseeda S, Parvathy PH, Sanjana AR and Anna Sebastine
+/******************************************************************************************************************************************************************************
+Github repo : 313849252
+Date : 20/05/2021
+Authors : Praseeda S, Sanjana AR, Parvathy PH, Anna Sebastine
+College Name : College of Engineering Trivandrum
+Project Name : Vriddhi : Design and Verification of RISC-V core
+Design name : RISC-V Testbench 
+Module name : riscv_tb
+Description : Inputs signals for the coordinated sequential simulation of proposed design without the need of any hardware and opens dump file for storing the state of design
+******************************************************************************************************************************************************************************/
+
 module riscv_tb(output out);
 
 reg  clk = 1'b0;
