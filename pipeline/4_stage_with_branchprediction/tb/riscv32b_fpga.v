65b11bf7294c47a4fbc570b502541ba1a83a480d
Anna Sebastine <65461848+SemoneSeb@users.noreply.github.com>
2021-06-21 10:49:23 +0530

    Added headers

--- pipeline/4_stage_with_branchprediction/tb/riscv32b_fpga.v
+++ pipeline/4_stage_with_branchprediction/tb/riscv32b_fpga.v
@@ -1,4 +1,14 @@
-//Owned by Praseeda S, Parvathy PH, Sanjana AR and Anna Sebastine
+/********************************************************************************************************
+Github repo : 313849252
+Date : 20/05/2021
+Authors : Praseeda S, Sanjana AR, Parvathy PH, Anna Sebastine
+College Name : College of Engineering Trivandrum
+Project Name : Vriddhi : Design and Verification of RISC-V core
+Design name : RISC-V FPGA Testbench 
+Module name : riscv32b_fpga
+Description : Testbench for core, ROM and RAM as a whole
+********************************************************************************************************/
+
 module riscv32b_fpga(
 input clk,
 input rstn,
